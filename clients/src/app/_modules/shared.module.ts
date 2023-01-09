07498f087add3e68dfd9d19471229c7794df3f81
Chris Binu <chris.binu@dedalus.eu>
2023-01-09 20:09:48 +0530

    End of Section 9.

--- clients/src/app/_modules/shared.module.ts
+++ clients/src/app/_modules/shared.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { TabsModule } from 'ngx-bootstrap/tabs';
+import { NgxGalleryModule } from '@kolkov/ngx-gallery';
 
 @NgModule({
   declarations: [],
-  imports: [CommonModule, BsDropdownModule],
-  exports: [BsDropdownModule]
+  imports: [
+    CommonModule,
+    BsDropdownModule,
+    TabsModule.forRoot(),
+    NgxGalleryModule,
+  ],
+  exports: [BsDropdownModule, TabsModule, NgxGalleryModule],
 })
 export class SharedModule {}
